529b823edea335b0dde8ede698dd1a107789477c
suke0828 <mwbspa@gmail.com>
2023-05-28 09:52:05 +0900

    ✨ Add use Marray to manipulate array data

--- src/Lib.hs
+++ src/Lib.hs
@@ -4,24 +4,39 @@
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE TypeOperators #-}
 
-module Lib (User, UserAPI, someFunc) where
+module Lib (User, UserAPI, runServer) where
 
+import Control.Monad.IO.Class (MonadIO (liftIO))
 import Data.Aeson ()
+import Data.Array.IO (IOArray)
+import Data.Array.MArray
+  ( MArray (getBounds),
+    getElems,
+    newListArray,
+    writeArray,
+  )
+import Data.IORef (IORef, newIORef, readIORef, writeIORef)
 import Elm.Derive qualified (defaultOptions, deriveBoth)
 import GHC.Generics (Generic)
 import Network.Wai.Handler.Warp (run)
-import Network.Wai.Middleware.Cors (simpleCors)
+import Network.Wai.Middleware.Cors (CorsResourcePolicy, cors, corsRequestHeaders, simpleCorsResourcePolicy, simpleHeaders)
 import Servant
   ( Application,
     Get,
+    Handler,
     JSON,
+    Post,
     Proxy (..),
+    ReqBody,
     Server,
     serve,
+    type (:<|>) (..),
     type (:>),
   )
 
-type UserAPI = "users" :> Get '[JSON] [User]
+type UserAPI =
+  "users" :> Get '[JSON] [User]
+    :<|> "users" :> ReqBody '[JSON] User :> Post '[JSON] ()
 
 data User = User
   { name :: String,
@@ -32,19 +47,43 @@ data User = User
 
 Elm.Derive.deriveBoth Elm.Derive.defaultOptions ''User
 
-users :: [User]
-users = [User "Isaac Newton" 372 "isaac@newton.co.uk"]
+server :: IORef (IOArray Int User) -> Server UserAPI
+server userArrayRef = getUsers :<|> postUser
+  where
+    getUsers :: Handler [User]
+    getUsers = do
+      userArray <- liftIO $ readIORef userArrayRef
+      liftIO $ getElems userArray
 
-server :: Server UserAPI
-server = return users
+    postUser :: User -> Handler ()
+    postUser user = do
+      userArray <- liftIO $ readIORef userArrayRef
+      liftIO $ do
+        lastIndex <- snd <$> getBounds userArray
+        newArray <- newListArray (0, lastIndex + 1) =<< getElems userArray
+        writeArray newArray (lastIndex + 1) user
+        writeIORef userArrayRef newArray
 
 userAPI :: Proxy UserAPI
 userAPI = Proxy
 
-app :: Application
-app = simpleCors $ serve userAPI server -- elm からのリクエストに対する cors の問題を回避する。
+policy :: CorsResourcePolicy
+policy =
+  simpleCorsResourcePolicy
+    { corsRequestHeaders = simpleHeaders
+    }
 
-someFunc :: IO ()
-someFunc = do
+app :: IORef (IOArray Int User) -> Application
+app userArrayRef = cors (const $ Just policy) $ serve userAPI (server userArrayRef) -- elm からのリクエストに対する cors の問題を回避する。
+
+runServer :: IO ()
+runServer = do
+  initialUsers <-
+    newListArray
+      (0, 1)
+      [ User "Isaac Newton" 372 "isaac@newton.co.uk",
+        User "Albert Einstein" 373 "ae@mc2.org"
+      ]
+  userArrayRef <- newIORef initialUsers
   putStrLn "Listening on port 8081"
-  run 8081 app
+  run 8081 (app userArrayRef)
