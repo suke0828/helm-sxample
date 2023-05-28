16b3da4f5fe8155be0d5bfc9f6dd53273974f144
suke0828 <mwbspa@gmail.com>
2023-05-28 09:46:08 +0900

    ✨ Add form

--- elm/src/Main.elm
+++ elm/src/Main.elm
@@ -1,8 +1,10 @@
 module Main exposing (..)
 
 import Browser
-import Generated.UserAPI exposing (User, getUsers)
-import Html exposing (Html, div, h1, li, text, ul)
+import Generated.UserAPI exposing (User, getUsers, postUsers)
+import Html exposing (Html, button, div, h1, input, li, text, ul)
+import Html.Attributes exposing (autocomplete, placeholder, type_, value)
+import Html.Events exposing (onClick, onInput)
 import Http
 
 
@@ -26,7 +28,14 @@ main =
 
 
 type alias Model =
-    { users : List User }
+    { users : List User, form : Form }
+
+
+type alias Form =
+    { name : String
+    , age : String
+    , email : String
+    }
 
 
 
@@ -35,7 +44,11 @@ type alias Model =
 
 init : ( Model, Cmd Msg )
 init =
-    ( { users = [] }, getUsers GotUsers )
+    ( { users = []
+      , form = { name = "", age = "", email = "" }
+      }
+    , getUsers GotUsers
+    )
 
 
 
@@ -45,16 +58,50 @@ init =
 
 type Msg
     = GotUsers (Result Http.Error (List User))
+    | SavedUser (Result Http.Error ())
+    | SaveUser
+    | Name String
+    | Age String
+    | Email String
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        GotUsers (Ok users) ->
-            ( { users = users }, Cmd.none )
+        Name name ->
+            ( { model | form = (\form -> { form | name = name }) model.form }, Cmd.none )
 
-        _ ->
-            ( model, Cmd.none )
+        Age age ->
+            ( { model | form = (\form -> { form | age = age }) model.form }, Cmd.none )
+
+        Email email ->
+            ( { model | form = (\form -> { form | email = email }) model.form }, Cmd.none )
+
+        SaveUser ->
+            let
+                inputData =
+                    { name = model.form.name
+                    , age = Maybe.withDefault 0 (String.toInt model.form.age)
+                    , email = model.form.email
+                    }
+            in
+            ( model, postUsers inputData SavedUser )
+
+        SavedUser result ->
+            case result of
+                Ok _ ->
+                    ( model, getUsers GotUsers )
+
+                Err _ ->
+                    ( model, getUsers GotUsers )
+
+        GotUsers result ->
+            case result of
+                Ok users ->
+                    ( { model | users = users }, Cmd.none )
+
+                Err _ ->
+                    ( model, Cmd.none )
 
 
 
@@ -66,6 +113,10 @@ view model =
     div []
         [ h1 [] [ text "Your Elm App is working!" ]
         , ul [] (List.map viewUsersList model.users)
+        , viewInput "text" "Name" model.form.name Name
+        , viewInput "text" "Age" model.form.age Age
+        , viewInput "text" "Email" model.form.email Email
+        , button [ onClick SaveUser ] [ text "Save" ]
         ]
 
 
@@ -79,3 +130,8 @@ viewUsersList user =
         , text ", Email: "
         , text user.email
         ]
+
+
+viewInput : String -> String -> String -> (String -> msg) -> Html msg
+viewInput t p v toMsg =
+    input [ type_ t, placeholder p, value v, onInput toMsg, autocomplete False ] []
